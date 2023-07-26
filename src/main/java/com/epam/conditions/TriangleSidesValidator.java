e62686dd50273f279673abec768f722047929a64
Muzaffar55 <97522350+Muzaffar55@users.noreply.github.com>
2023-07-26 22:42:47 +0500

    Update TriangleSidesValidator.java
    Done

--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -1,9 +1,15 @@
 package com.epam.conditions;
 
 public class TriangleSidesValidator {
-
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+        if (firstSide + secondSide < thirdSide) {
+            System.out.println("it's not a triangle");
+        } else if (firstSide + thirdSide < secondSide) {
+            System.out.println("it's not a triangle");
+        } else if (thirdSide + secondSide < firstSide) {
+            System.out.println("it's not a triangle");
+        } else {
+            System.out.println("this is a valid triangle");
+        }
     }
-
 }
