f06704f2a44f6d5668bceb39e75b62ae1abbbb02
Muzaffar55 <97522350+Muzaffar55@users.noreply.github.com>
2023-07-26 22:38:00 +0500

    Update Pyramid.java
    Done

--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -1,13 +1,22 @@
 package com.epam.loops;
 
 public class Pyramid {
-
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j < cathetusLength + i; j++) {
+                if (i > cathetusLength - j && j < cathetusLength ) {
+                    System.out.print(cathetusLength - j + 1);
+                } else if ( j > cathetusLength - 1 && cathetusLength > j - cathetusLength) {
+                    System.out.print(j - cathetusLength + 1);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
     }
-
 }
